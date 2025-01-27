ee420043b1c5019d9da765ace6fbe9651f7441e5
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-01-26 19:13:39 -0700

    lint route

--- app/api/characters/route.ts
+++ app/api/characters/route.ts
@@ -1,30 +1,20 @@
 import { NextResponse } from "next/server";
-import { Character } from "@/app/types";
 import { clerkClient } from "@clerk/nextjs/server";
 
 export async function GET() {
   try {
-    // Initialize Clerk client
     const clerk = await clerkClient();
 
-    // Fetch all users from Clerk
     const usersResponse = await clerk.users.getUserList();
 
-    // Access the `data` property to get the array of users
     const users = usersResponse.data;
 
-    // Extract characters from each user's public metadata
-    const characters = users.map((user: any) => {
+    const characters = users.map((user) => {
       const userMetadata = user.publicMetadata || {};
       return userMetadata.character || null;
     });
 
-    // Filter out null values (users without characters)
-    const validCharacters = characters.filter(
-      (character: Character): character is Character => character !== null
-    );
-
-    return NextResponse.json({ characters: validCharacters });
+    return NextResponse.json({ characters: characters });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
