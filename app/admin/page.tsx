c8293731230da6a5364e821eb8b810482d13acf8
Charles Wefso <61127453+cwefso@users.noreply.github.com>
2025-02-07 19:16:39 -0700

    mobile admin grid col 1

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <div>
       <h1>Character Sheets</h1>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         {uniqueCharacters.map((character, index) => (
           <CharacterSheet key={index} character={character} />
         ))}
